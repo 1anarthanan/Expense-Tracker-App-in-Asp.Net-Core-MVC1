16573507f1ce9493ec9aa7e981576f37c1093fde
jana <janarthanangandhi2001@gmail.com>
2023-01-22 19:48:58 +0530

    Fist commit

---
+++ Expense Tracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
